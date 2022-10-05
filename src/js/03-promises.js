a7009692424c32981845fe300718abc54a78f7a7
Xray1221 <105057432+Xray1221@users.noreply.github.com>
2022-10-05 14:53:47 +0300

    completed

---
+++ src/js/03-promises.js
@@ -0,0 +1,38 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+refs = {
+  delay: document.querySelector('[name="delay"]'),
+  step: document.querySelector('[name="step"]'),
+  amount: document.querySelector('[name="amount"]'),
+  submitBtn: document.querySelector('button[type="submit"]')
+};
+
+refs.submitBtn.addEventListener('click', onSubmit);
+
+function onSubmit(event){
+  event.preventDefault();
+  const amount = +refs.amount.value;
+  const delay = +refs.delay.value;
+  const step = +refs.step.value;
+  for(let i = 0; i < amount; i++){
+    createPromise(i + 1, delay + (i * step)).then(({ position, delay }) => {
+      Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+  }
+}
+
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({position, delay})
+      } else {
+        reject({position, delay})
+      }
+    }, delay);
+  });
+}
